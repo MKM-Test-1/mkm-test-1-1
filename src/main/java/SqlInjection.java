33452badbbd872cac8a6b2ad6fd5c08ec1fc162c
mathkraemer <43319514+mathkraemer@users.noreply.github.com>
2026-09-24 21:06:05 +0200

    update file src/main/java/SqlInjection.java

--- src/main/java/SqlInjection.java
+++ src/main/java/SqlInjection.java
@@ -7,18 +7,18 @@ public class SqlInjection {
     public static ResultSet doQuery(HttpServletRequest request, Connection connection) throws SQLException {
         String customerName = request.getParameter("customerName");
         String query = "SELECT account_balance FROM user_data WHERE user_name = ?";
-        PreparedStatement statement = connection.prepareStatement(query);
-        statement.setString(1, customerName);
-        ResultSet results = statement.executeQuery();
-        return(results);
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, customerName);
+            return(statement.executeQuery());
+        }
     }
 
     public static ResultSet doQuery2(HttpServletRequest request, Connection connection) throws SQLException {
         String customerName = request.getParameter("customerName");
         String query = "SELECT account_balance FROM user_data WHERE user_name = ?";
-        PreparedStatement statement = connection.prepareStatement(query);
-        statement.setString(1, customerName);
-        ResultSet results = statement.executeQuery();
-        return(results);
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, customerName);
+            return(statement.executeQuery());
+        }
     }
 }
