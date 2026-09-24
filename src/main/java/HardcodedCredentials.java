2993f122d7b43f9f3204fb3d44ca0155c9661616
mathkraemer <43319514+mathkraemer@users.noreply.github.com>
2026-09-24 21:21:35 +0200

    update file src/main/java/HardcodedCredentials.java

--- src/main/java/HardcodedCredentials.java
+++ src/main/java/HardcodedCredentials.java
@@ -12,7 +12,7 @@ public class HardcodedCredentials {
             /*password*/ "jenkins");
     }
     public static SecretKeySpec getKeySpec() throws UnsupportedEncodingException {
-        String secret = "It's a secret to everybody.";
+        String secret = System.getProperty("app.secret.key");
         SecretKeySpec keyspec = new SecretKeySpec(secret.getBytes("UTF-8"), "MD5");
         return(keyspec);
     }
