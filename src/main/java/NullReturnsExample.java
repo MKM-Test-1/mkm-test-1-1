1e2bb051ea44d878e01cbb7948ea417f088e4c96
Matthias Kraemer <matthias.kraemer@blackduck.com>
2026-09-25 09:44:25 +0200

    Demo another commit

--- src/main/java/NullReturnsExample.java
+++ src/main/java/NullReturnsExample.java
@@ -1,12 +1,13 @@
 // https://documentation.blackduck.com/bundle/coverity-docs/page/checker-ref/checkers/NO/null_returns.html
 
 public class NullReturnsExample {
+    
     static int count = 0;
 
     public static Object returnA() {
         return null;
     }
-    
+
     public static Object returnB() {
         return new Object();
     }
