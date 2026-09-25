9aed44b60426daca0c29e03159c8b80885408266
Matthias Kraemer <matthias.kraemer@blackduck.com>
2026-09-25 10:12:44 +0200

    Small code change for demo.

--- src/main/java/NullReturnsExample.java
+++ src/main/java/NullReturnsExample.java
@@ -1,7 +1,8 @@
 // https://documentation.blackduck.com/bundle/coverity-docs/page/checker-ref/checkers/NO/null_returns.html
 
+
 public class NullReturnsExample {
-    
+
     static int count = 0;
 
     public static Object returnA() {
