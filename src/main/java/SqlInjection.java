567c68c98586b73c2603ace1e5994acdce76595c
Matthias Kraemer <matthias.kraemer@blackduck.com>
2026-09-25 09:30:55 +0200

    Restoring SqlInjection

--- src/main/java/SqlInjection.java
+++ src/main/java/SqlInjection.java
@@ -6,19 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 public class SqlInjection {
     public static ResultSet doQuery(HttpServletRequest request, Connection connection) throws SQLException {
         String customerName = request.getParameter("customerName");
-        String query = "SELECT account_balance FROM user_data WHERE user_name = ?";
-        PreparedStatement statement = connection.prepareStatement(query);
-        statement.setString(1, customerName);
-        ResultSet results = statement.executeQuery();
+        String query = "SELECT account_balance FROM user_data WHERE user_name = " + customerName;
+        Statement statement = connection.createStatement();
+        ResultSet results = statement.executeQuery(query);
         return(results);
     }
-
-    public static ResultSet doQuery2(HttpServletRequest request, Connection connection) throws SQLException {
-        String customerName = request.getParameter("customerName");
-        String query = "SELECT account_balance FROM user_data WHERE user_name = ?";
-        PreparedStatement statement = connection.prepareStatement(query);
-        statement.setString(1, customerName);
-        ResultSet results = statement.executeQuery();
-        return(results);
-    }
-}
